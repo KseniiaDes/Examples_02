de740bc473c625049351fb5828d308031ac59a6b
Foxy <kseniya250496@mail.ru>
2022-11-20 14:28:12 +0300

    add Example008 METHOD

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 123235;
+int b1 = 3442;
+int c1 = 23;
+int a2 = 15;
+int b2 = 32;
+int c2 = 233;
+int a3 = 15;
+int b3 = 32;
+int c3 = 2223;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1, b1, c1), 
+    Max(a2, b2, c2), 
+    Max(a3, b3, c3));
+
+Console.WriteLine(max);
